51612c17b8815e8733346d64fa738b86bceac24e
PoojaKarnik195 <41577579+PoojaKarnik195@users.noreply.github.com>
2018-08-05 19:48:30 +0530

    Add files via upload

---
+++ Day-10-MVC and Layering Application/MoneyMoneyBankApp/src/com/moneymoney/app/model/dao/MMBankCollection.java
@@ -0,0 +1,16 @@
+package com.moneymoney.app.model.dao;
+
+import com.moneymoney.framework.account.dao.BankAccountCollection;
+
+/**
+ * @author Pooja Karnik
+ * 
+ *         Inherits the properties of BankApp
+ *
+ */
+public class MMBankCollection extends BankAccountCollection {
+
+	public MMBankCollection() {
+		super();
+	}
+}
